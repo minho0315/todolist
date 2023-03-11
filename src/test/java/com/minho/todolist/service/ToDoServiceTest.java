b3ef7f9d01f7bcd4f6e3c12a43ddde8a023403ba
minho0315 <dlalsgh369@naver.com>
2023-03-11 12:40:38 +0900

    ToDoServiceTest 작성

--- src/test/java/com/minho/todolist/service/ToDoServiceTest.java
+++ src/test/java/com/minho/todolist/service/ToDoServiceTest.java
@@ -7,23 +7,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
 public class ToDoServiceTest {
+
     @Autowired ToDoService toDoService;
 
     @Test
-    public void 저장() {
+    public void saveToDo() {
 
-        //Given
-        ToDo toDo = new ToDo();
-        toDo.setState(false);
-        toDo.setContent("공부하기");
+        //given
+        ToDo toDo = createToDo("공부하기", false);
 
-        //When
+        //when
         Long savedToDoId = toDoService.saveToDo(toDo);
 
-        //Then
-        Assertions.assertThat(toDo.getId()).isEqualTo(savedToDoId);
+        //then
+        assertThat(toDo.getId()).isEqualTo(savedToDoId);
+    }
+
+    @Test
+    public void findOne() {
+
+        //given
+        ToDo toDo = createToDo("findOne", false);
+        Long savedToDoId = toDoService.saveToDo(toDo);
+
+        //when
+        ToDo findToDo = toDoService.findOne(savedToDoId);
+
+        //then
+        assertThat(savedToDoId).isEqualTo(findToDo.getId());
+    }
+
+    @Test
+    public void findToDos() {
+
+        //given
+        ToDo toDo1 = createToDo("test1", false);
+        ToDo toDo2 = createToDo("test2", false);
+        toDoService.saveToDo(toDo1);
+        toDoService.saveToDo(toDo2);
+
+        //when
+        List<ToDo> toDos = toDoService.findToDos();
+
+        //then
+        assertThat(2).isEqualTo(toDos.size());
+    }
+
+    @Test
+    public void updateToDo() {
+
+        //given
+        ToDo toDo = createToDo("test", false);
+        Long savedToDoId = toDoService.saveToDo(toDo);
+
+        //when
+        toDoService.updateToDo(toDo.getId(), "test1", true);
+
+        //then
+        assertThat("test1").isEqualTo(toDo.getContent());
+        assertThat(true).isEqualTo(toDo.getState());
+    }
+
+    @Test
+    public void cancelToDo() {
+
+        //given
+        ToDo toDo = createToDo("test", false);
+        Long savedToDoId = toDoService.saveToDo(toDo);
+
+        //when
+        toDoService.cancelTodo(savedToDoId);
+        List<ToDo> toDos = toDoService.findToDos();
+
+        //then
+        assertThat(0).isEqualTo(toDos.size());
+    }
+
+
+
+    private ToDo createToDo(String content, boolean state) {
+        ToDo toDo = new ToDo();
+        toDo.setContent(content);
+        toDo.setState(state);
+        return toDo;
     }
 }
